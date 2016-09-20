61efaa884016a657fa6be11b6307fffbc1fe6cd5
sseaver <sseaver321@gmail.com>
2016-09-20 09:02:16 -0400

    final comprehensions hw

---
+++ nested_comprehensions.py
@@ -0,0 +1,9 @@
+
+
+def average_grade():
+    hw_dict = {'Gale': {'Homework 1': 88, 'Homework 2': 76},
+               'Jordan': {'Homework 1': 92, 'Homework 2': 87},
+               'Peyton': {'Homework 1': 84, 'Homework 2': 77},
+               'River': {'Homework 1': 85, 'Homework 2': 91}
+               }
+    hw_one = 
